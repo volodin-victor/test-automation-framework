f0e70c05c82e7486798d53cdca2d05c58fcaf755
volodin-victor <Pb3CFg@dm1N!>
2021-10-09 10:20:40 -0700

    Added page classes with steps and initialization for Login and Main pages

---
+++ src/test/java/net/volodin/pages/LoginPage.java
@@ -0,0 +1,24 @@
+package net.volodin.pages;
+
+import net.volodin.testbase.BaseClass;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage {
+    @FindBy(id = "user-name")
+    public WebElement userName;
+
+    @FindBy(id = "password")
+    public WebElement password;
+
+    @FindBy(id = "login-button")
+    public WebElement loginBtn;
+
+    @FindBy(xpath = "//h3[@data-test='error']")
+    public WebElement errorMsg;
+
+    public LoginPage() {
+        PageFactory.initElements(BaseClass.driver, this);
+    }
+}
