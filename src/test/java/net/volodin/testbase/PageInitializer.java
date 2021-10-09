f12d68db7edd4c5c7e3eb20afae9b9cc4ec60de3
volodin-victor <Pb3CFg@dm1N!>
2021-10-09 10:20:40 -0700

    Added page classes with steps and initialization for Login and Main pages

--- src/test/java/net/volodin/testbase/PageInitializer.java
+++ src/test/java/net/volodin/testbase/PageInitializer.java
@@ -1,11 +1,17 @@
 package net.volodin.testbase;
 
+import net.volodin.pages.LoginPage;
+import net.volodin.pages.MainPage;
+
 /**
  * Method performs initialization
  */
 public class PageInitializer extends BaseClass {
+    public static LoginPage loginPage;
+    public static MainPage mainPage;
 
     public static void initialize() {
-
+        loginPage = new LoginPage();
+        mainPage = new MainPage();
     }
 }
