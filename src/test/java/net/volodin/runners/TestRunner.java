ded1d5eba0c09f8a00d5a2fc280a59b8194a8ca5
volodin-victor <Pb3CFg@dm1N!>
2021-10-09 10:20:31 -0700

    Added TestRunner class

---
+++ src/test/java/net/volodin/runners/TestRunner.java
@@ -0,0 +1,23 @@
+package net.volodin.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/resources/features",
+        glue = "net.volodin.steps",
+        dryRun = false,
+        tags = "@login",
+        monochrome = true,
+        plugin = {
+                "pretty",
+                "html:target/cucumber-default-report",
+                "json:target/cucumber.json",
+                "rerun:target/failed.txt"
+        }
+)
+
+public class TestRunner {
+}
